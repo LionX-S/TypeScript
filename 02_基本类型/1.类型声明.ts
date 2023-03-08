af11f0edc8461638c28f20f4dabb9e3286b9a1f7
LionX-S <13141096848@163.com>
2023-03-08 18:18:58 +0800

    feat：TS初识

---
+++ 02_基本类型/1.类型声明.ts
@@ -0,0 +1,15 @@
+let a: number;
+a = 10;
+a = 20;
+// a = '12'; 错误 不能将string型给number型
+
+let c: boolean = true;
+let d = 12;
+// c = 12;
+// d = ''
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2
+}
+
+let result = sum(12, 1)
