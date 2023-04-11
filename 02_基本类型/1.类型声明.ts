a47fdbe55d2657f9abe4e141f54af5910b8701ba
LionX-S <13141096848@163.com>
2023-04-11 14:43:06 +0800

    feat: update typescript type

--- 02_基本类型/1.类型声明.ts
+++ 02_基本类型/1.类型声明.ts
@@ -29,11 +29,11 @@ e = "123";
 let f: string;
 // 断言
 f = e as string;
-f = <string> e;
+f = <string>e;
 
-let g: (a:number,b:number) => number;
-g = function(num1,num2) {
-  return num1+num2
+let g: (a: number, b: number) => number;
+g = function (num1, num2) {
+  return num1 + num2
 }
 
 // 枚举 值为1或0
@@ -41,5 +41,76 @@ enum Gender {
   male,
   female
 }
-let person:{name:string,age:number,gender:Gender};
-person = {name:'jack',age:18,gender:Gender.female}
+let person: { name: string, age: number, gender: Gender };
+person = { name: 'jack', age: 18, gender: Gender.female }
+
+// 如果在声名时不指定类型，TS会做类型推断，推断成为any
+let something;
+something = '123';
+something = 7;
+
+// 一旦定义了任意属性，那么确定属性和可选属性的类型都必须是它的类型的子集
+interface Person {
+  name: string;
+  // age: number;
+  [propName: string]: string;
+}
+
+// 定义数组
+let hobbies: string[] = ['guitar', 'code'];
+// 数组的一些方法的参数也会根据数组在定义时约定的类型进行限制：
+// hobbies.push(12)
+// 泛型定义
+let eat: Array<string> = ['apple'];
+// 接口定义
+interface NumberArray {
+  [index:number]: number
+}
+let numberArray:NumberArray = [1,2,3]
+
+// 函数表达式类型声名
+let number: (x: number, y: number) => number = function (x: number, y: number): number {
+  return x + y;
+}
+// 也可以用接口定义函数
+interface SearchFunc {
+  (source:string, subString: string): boolean
+}
+let mySearch: SearchFunc;
+mySearch = function(source:string, subString: string) {
+  return false
+}
+
+// 断言
+// 将父类断言为更具体的子类
+class ApiError extends Error {
+  code:number = 0;
+}
+
+class HttpError extends Error {
+  apiCode:number = 200;
+}
+
+function isApiError(error: Error):boolean{
+  if(typeof (error as ApiError).code === 'number') {
+    return true
+  }
+  return false
+}
+
+// 断言为any
+// window.foo = 1;
+(window as any).foo = 1;
+
+// 元祖
+let tom:[string, number] = ['tom', 20];
+
+// 泛型
+function createArray<T>(length:number,value: T): Array<T> {
+  let result: T[] = [];
+  for (let i = 0; i < length; i++) {
+    result[i] = value
+  }
+  return result
+}
+createArray<string>(2,'x');
